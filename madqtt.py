9a6517f15c20e3b162977322343fd35ff549e0a5
techolutions <andre.wagner86@gmail.com>
2023-02-20 19:52:41 +0100

    some things

--- madqtt.py
+++ madqtt.py
@@ -51,8 +51,8 @@ class MADqtt(mapadroid.plugins.pluginBase.Plugin):
         self._mad_parts['logger'].info('plugin is running on instance {0}'.format(self._instance))
 
         await self.load_plugin_config()
-        await self.save_plugin_config()
-        await self.search_devices()
+        # await self.save_plugin_config()
+        # await self.search_devices()
 
         self.event_loop()
 
@@ -61,87 +61,88 @@ class MADqtt(mapadroid.plugins.pluginBase.Plugin):
     async def load_plugin_config(self):
         self._mad_parts['logger'].info('load_plugin_config')
         self._config = {
-            'topic': self._pluginconfig.get('mqtt', 'topic', fallback='madqtt'),
-            'broker': {
-                'host': self._pluginconfig.get('broker', 'host', fallback='localhost'),
-                'port': int(self._pluginconfig.get('broker', 'port', fallback=1883)),
-                'user': self._pluginconfig.get('broker', 'user', fallback=None),
-                'pass': self._pluginconfig.get('broker', 'pass', fallback=None)
-            },
             'timeouts': {
                 'mitm': int(self._pluginconfig.get('timeouts', 'mitm', fallback=600)),
                 'proto': int(self._pluginconfig.get('timeouts', 'proto', fallback=600)),
                 'restart': int(self._pluginconfig.get('timeouts', 'restart', fallback=900)),
                 'check': int(self._pluginconfig.get('timeouts', 'check', fallback=60))
+            },
+            'mqtt': {
+                'host': self._pluginconfig.get('mqtt', 'host', fallback='localhost'),
+                'port': int(self._pluginconfig.get('mqtt', 'port', fallback=1883)),
+                'user': self._pluginconfig.get('mqtt', 'user', fallback=None),
+                'pass': self._pluginconfig.get('mqtt', 'pass', fallback=None),
+                'ssl': self._pluginconfig.getboolean("mqtt", "ssl", fallback=False),
+                'client-id': self._pluginconfig.get('mqtt', 'client-id', fallback='madqtt-client')
             }
         }
         self._mad_parts['logger'].info(self._config)
 
-    async def save_plugin_config(self):
-        self._mad_parts['logger'].info('save_plugin_config')
-        self._pluginconfig.set('timeouts', 'check', '30')
-
-        with open(self._rootdir + "/plugin.ini", "w") as configfile:
-            self._pluginconfig.write(configfile)
-
-        self.load_plugin_config()
-
-    async def search_devices(self):
-        self._mad_parts['logger'].info('search_devices')
-
-        self._devices = []
-        async with self._mad_parts["db_wrapper"] as session, session:
-            for settingsDevice in await SettingsDeviceHelper.get_all(session, self._instance):
-                device = {}
-                device['id'] = settingsDevice.device_id
-                device['origin'] = settingsDevice.name
-                device['state'] = None
-                device['power-time'] = None
-                self._devices.append(device)
-
-        self._mad_parts['logger'].info(self._devices)
-
-    async def refresh_devices(self):
-        self._mad_parts['logger'].info('refresh_devices')
-
-        for device in self._devices:
-            device['injected'] = await self._mad_parts['mitm_mapper'].get_injection_status(device['origin'])
-
-            async with self._mad_parts["db_wrapper"] as session, session:
-                trsStatus: Optional[TrsStatus] = await TrsStatusHelper.get(session, device['id'])
-                if trsStatus:
-                    device['idle'] = trsStatus.idle
-                    device['proto-time'] = trsStatus.lastProtoDateTime
-                    device['sleep-time'] = trsStatus.currentSleepTime
-                    device['softban-time'] = trsStatus.last_softban_action
-                else:
-                    device['idle'] = None
-                    device['proto-time'] = None
-                    device['sleep-time'] = None
-                    device['softban-time'] = None
-
-        self._mad_parts['logger'].info(self._devices)
+    # async def save_plugin_config(self):
+    #     self._mad_parts['logger'].info('save_plugin_config')
+    #     self._pluginconfig.set('timeouts', 'check', '30')
+    #
+    #     with open(self._rootdir + "/plugin.ini", "w") as configfile:
+    #         self._pluginconfig.write(configfile)
+    #
+    #     self.load_plugin_config()
+    #
+    # async def search_devices(self):
+    #     self._mad_parts['logger'].info('search_devices')
+    #
+    #     self._devices = []
+    #     async with self._mad_parts["db_wrapper"] as session, session:
+    #         for settingsDevice in await SettingsDeviceHelper.get_all(session, self._instance):
+    #             device = {}
+    #             device['id'] = settingsDevice.device_id
+    #             device['origin'] = settingsDevice.name
+    #             device['state'] = None
+    #             device['power-time'] = None
+    #             self._devices.append(device)
+    #
+    #     self._mad_parts['logger'].info(self._devices)
+    #
+    # async def refresh_devices(self):
+    #     self._mad_parts['logger'].info('refresh_devices')
+    #
+    #     for device in self._devices:
+    #         device['injected'] = await self._mad_parts['mitm_mapper'].get_injection_status(device['origin'])
+    #
+    #         async with self._mad_parts["db_wrapper"] as session, session:
+    #             trsStatus: Optional[TrsStatus] = await TrsStatusHelper.get(session, device['id'])
+    #             if trsStatus:
+    #                 device['idle'] = trsStatus.idle
+    #                 device['proto-time'] = trsStatus.lastProtoDateTime
+    #                 device['sleep-time'] = trsStatus.currentSleepTime
+    #                 device['softban-time'] = trsStatus.last_softban_action
+    #             else:
+    #                 device['idle'] = None
+    #                 device['proto-time'] = None
+    #                 device['sleep-time'] = None
+    #                 device['softban-time'] = None
+    #
+    #     self._mad_parts['logger'].info(self._devices)
 
     async def madqtt(self):
         while True:
             self._mad_parts['logger'].info('searching for devices that need a reboot')
 
-            await self.refresh_devices()
-            for device in self._devices:
-                if device['state'] == 'off':
-                    # turned off devices should be skipped
-                    self._mad_parts['logger'].info('{0} has been skipped, because it\'s off'.format(device['origin']))
-                elif device['idle'] == 1:
-                    # paused devices should be skipped
-                    self._mad_parts['logger'].info('{0} has been skipped, because it\'s paused'.format(device['origin']))
-
-                elif (datetime.datetime.now(datetime.timezone.utc) - device['power-time']) < datetime.timedelta(seconds=self._config['timeouts']['restart']):
-                    # recently restarted devices sholud be skipped
-                    self._mad_parts['logger'].info('{0} has been skipped, because it\'s recently (re)started'.format(device['origin']))
-                # elif (device['injected'] == False and self.elapsed_seconds(device['data-time']) > self._config['timeouts']['mitm']) or (self.elapsed_seconds(device['proto-time'] + device['sleep-time']) > self._config['timeouts']['proto']):
-                #     self._logger.info('device {0} will be restarted'.format(device['origin']))
-                #     self.device_command(device['origin'], 'restart')
-                #     time.sleep(1)
+            # await self.refresh_devices()
+            # for device in self._devices:
+            #     if device['state'] == 'off':
+            #         # turned off devices should be skipped
+            #         self._mad_parts['logger'].info('{0} has been skipped, because it\'s off'.format(device['origin']))
+            #     elif device['idle'] == 1:
+            #         # paused devices should be skipped
+            #         self._mad_parts['logger'].info('{0} has been skipped, because it\'s paused'.format(device['origin']))
+            #
+            #     elif (datetime.datetime.now(datetime.timezone.utc) - device['power-time']) < datetime.timedelta(seconds=self._config['timeouts']['restart']):
+            #         # recently restarted devices sholud be skipped
+            #         self._mad_parts['logger'].info('{0} has been skipped, because it\'s recently (re)started'.format(device['origin']))
+            #     elif (device['injected'] == False and self.elapsed_seconds(device['data-time']) > self._config['timeouts']['mitm']) or (self.elapsed_seconds(device['proto-time'] + device['sleep-time']) > self._config['timeouts']['proto']):
+            #         self._logger.info('device {0} will be restarted'.format(device['origin']))
+            #         self.device_command(device['origin'], 'restart')
+            #         time.sleep(1)
 
             await asyncio.sleep(self._config['timeouts']['check'])
 
